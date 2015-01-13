8b2fcd2f166067af33a19c82791a9cf86d48879d
LarsBB <lars.syfuss@googlemail.com>
2015-01-13 20:31:56 +0100

    Filter work and Trackscollection is created

--- shiny/fcd/server.R
+++ shiny/fcd/server.R
@@ -1,30 +1,95 @@
 # server.R
 
+source ("import.R")
+
+# Functions
+changeTrackSelection = function(){
+  # get length of current tracksCollection
+  len = length(trCol@tracksCollection)
+  numbers = c(1:len)
+  tracknames = paste("track", numbers)
+  # assign tracknames to tracksList
+  selectInput("tracksList", 
+              label = "Choose a Track to display",
+              choices = tracknames,
+              selected = tracknames[1])
+}
+
+create_bbox_from_input = function(ll, ur){
+  return(matrix(c(ll, ur),ncol=2,dimnames=list(c("x","y"),c("min","max"))))
+}
+
+create_timeinterval_from_input = function(start, end){
+  start = as.POSIXct(start)
+  end = as.POSIXct(end)
+  return(c(start,end))
+}
+
+# create default trackcollection
+stableURL = "https://envirocar.org/api/stable"
+# create default bbox for Muenster
+ll = c(7.6,51.95) # lower left : 51.928179, 7.573629
+ur = c(7.65,52) # upper right: 51.985515, 7.674909
+default_bbox = matrix(c(ll, ur),ncol=2,dimnames=list(c("x","y"),c("min","max")))
+trCol = importEnviroCar(serverUrl = stableURL, bbox = default_bbox, limit = 1)
+
 shinyServer(function(input, output, session) {
   
   # create leaflet map
-  map <- createLeafletMap(session, "map")
-  
+  map = createLeafletMap(session, "map")
   
   # output selected dates:
-  output$selectedDates <- renderText({
-    paste("First date: ", input$dates[1], "\n Second date: ", input$dates[2])
-  })
-  
-  # output the selected track:
-  output$selectedTracksList <- renderText({ 
-    paste("Your selection: ", input$tracksList)
+  output$selectedDates = renderText({
+    paste("Dates: \n First date: ", input$dates[1], "\n Second date: ", input$dates[2])
   })
   
   # output the boundingbox:
-  output$boundingBoxText <- renderText({ 
-    NE_LAT <- paste("NE_LAT", input$map_bounds[1])
-    NE_LON <- paste("NE_LON", input$map_bounds[2])
-    SW_LAT <- paste("SW_LAT", input$map_bounds[3])
-    SW_LON <- paste("SW_LON", input$map_bounds[4])
-    
-    HTML(paste(NE_LAT, NE_LON, SW_LAT, SW_LON, sep = '<br/>'))
-    
+  output$boundingBoxText = renderText({
+    if(input$checkbox[1]){
+      TEXT = "Boundingbox:"
+      NE_LAT = paste("NE_LAT", input$map_bounds[1])
+      NE_LON = paste("NE_LON", input$map_bounds[2])
+      SW_LAT = paste("SW_LAT", input$map_bounds[3])
+      SW_LON = paste("SW_LON", input$map_bounds[4])
+      paste(TEXT, "\n", NE_LAT, "\n", NE_LON, "\n", SW_LAT, "\n", SW_LON, "\n")
+    } else {
+      paste("BBox not selected!", "\n")
+    }
+  })
+  
+  # output the selected limit
+  output$limit = renderPrint({ input$limit_slider })
+  
+  # observe if button is clicked!
+  observe({
+    if (input$search_btn == 0) 
+      return()
+    isolate({
+      time = create_timeinterval_from_input(input$dates[1], input$dates[2])
+      print(paste("time -> ", time))
+      limit = input$limit_slider
+      print(paste("limit -> ", limit))
+      # just get the bbox if the checkbox is clicked!
+      if(input$checkbox[1]){
+        bbox = create_bbox_from_input(c(input$map_bounds[4],input$map_bounds[3]), c(input$map_bounds[2],input$map_bounds[1]))
+        print(paste("bbox -> ", bbox))
+        trCol <<- importEnviroCar(serverUrl = stableURL, bbox = bbox, timeInterval = time, limit = limit)
+      }
+      # else do not consider the bbox
+      else {
+        trCol <<- importEnviroCar(serverUrl = stableURL, timeInterval = time, limit = limit)
+      }
+      print("Tracks loaded!")
+      # make choosable tracks refering to trackscollection
+      output$tracks = renderUI({
+        changeTrackSelection()
+      })
+    })
+  })
+  
+  # output the selected track name:
+  output$selectedTracksList = renderText({ 
+    paste("Selected Track: ", "\n", input$tracksList)
   })
 
 })
